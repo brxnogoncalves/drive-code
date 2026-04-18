032d84254ca3a8b04364a6c8bb4ac25239a728b1
brxnogoncalves <bruno.goncalves@sptech.school>
2026-04-18 00:28:05 -0300

    remove pagina de login e cadastro

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -16,8 +16,7 @@ var HOST_APP = process.env.APP_HOST;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var carrosRouter = require("./src/routes/carros");
+// var usuarioRouter = require("./src/routes/usuarios");
 // var avisosRouter = require("./src/routes/avisos");
 // var medidasRouter = require("./src/routes/medidas");
 // var aquariosRouter = require("./src/routes/aquarios");
@@ -30,8 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/carros", carrosRouter);
+// app.use("/usuarios", usuarioRouter);
 // app.use("/avisos", avisosRouter);
 // app.use("/medidas", medidasRouter);
 // app.use("/aquarios", aquariosRouter);
--- web-data-viz/src/controllers/not_used/usuarioController.js
+++ web-data-viz/src/controllers/not_used/usuarioController.js
@@ -52,40 +52,41 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    // var nome = req.body.nomeServer;
-    // var email = req.body.emailServer;
-    // var senha = req.body.senhaServer;
-    // var fkEmpresa = req.body.idEmpresaVincularServer;
+    let nome = req.body.nomeServer;
+    let email = req.body.emailServer;
+    let senha = req.body.senhaServer;
+    let dtNascimento = req.body.dtNascimentoServer;
+    let fkCarroPreferido = req.body.fkCarroPreferidoServer;
 
-    // // Faça as validações dos valores
-    // if (nome == undefined) {
-    //     res.status(400).send("Seu nome está undefined!");
-    // } else if (email == undefined) {
-    //     res.status(400).send("Seu email está undefined!");
-    // } else if (senha == undefined) {
-    //     res.status(400).send("Sua senha está undefined!");
-    // } else if (fkEmpresa == undefined) {
-    //     res.status(400).send("Sua empresa a vincular está undefined!");
-    // } else {
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (dtNascimento == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
+    } else if (fkCarroPreferido == undefined) {
+        res.status(400).send("Seu carro preferido está undefined!");
+    } else {
 
-    //     // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    //     usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
-    //         .then(
-    //             function (resultado) {
-    //                 res.json(resultado);
-    //             }
-    //         ).catch(
-    //             function (erro) {
-    //                 console.log(erro);
-    //                 console.log(
-    //                     "\nHouve um erro ao realizar o cadastro! Erro: ",
-    //                     erro.sqlMessage
-    //                 );
-    //                 res.status(500).json(erro.sqlMessage);
-    //             }
-    //         );
-    // }
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, email, senha, dtNascimento, fkCarroPreferido)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 }
 
 module.exports = {
