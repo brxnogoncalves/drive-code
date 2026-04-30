55b14062b3ea43b5a63a39eca0499aa6c877fd6e
brxnogoncalves <bruno.goncalves@sptech.school>
2026-04-30 12:23:50 -0300

    alterações no banco de dados

--- banco-de-dados/seed.sql
+++ banco-de-dados/seed.sql
@@ -4,7 +4,7 @@ USE drivecode;
 CREATE TABLE usuario (
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30),
-    email VARCHAR(80),
+    email VARCHAR(80) UNIQUE,
     senha VARCHAR(255),
     data_nascimento DATE
 );
@@ -12,10 +12,27 @@ CREATE TABLE usuario (
 CREATE TABLE carro_usuario (
     id_carro_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome_carro VARCHAR(45),
-    cor VARCHAR(30) DEFAULT '00000000', -- ARMAZENA HEX DA COR
+    cor CHAR(7) DEFAULT '#000000', -- ARMAZENA HEX DA COR
     nivel INT DEFAULT 1,
     xp INT DEFAULT 0,
     fk_usuario INT UNIQUE,
     CONSTRAINT fkCarroUsuario
         FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE game (
+	id_game INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(60),
+    descricao VARCHAR(255),
+    xp_recompensa INT
+);
+
+CREATE TABLE historico_xp (
+	id_historico_xp INT PRIMARY KEY AUTO_INCREMENT,
+    xp_ganho INT,
+    data_ganho DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fk_usuario INT,
+    fk_game INT,
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (fk_game) REFERENCES game(id_game)
 );
