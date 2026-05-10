1f7b4d35c87b82ba0f94b766b07c925dc15751c9
brxnogoncalves <bruno.goncalves@sptech.school>
2026-05-10 17:06:23 -0300

    adiciona pagina de motor ao codigo

--- web-data-viz/public/assets/js/utils.js
+++ web-data-viz/public/assets/js/utils.js
@@ -15,10 +15,17 @@ function alterarNavBar() {
         <li class='usuario-logado'>Olá, ${nomeUsuario}</li>
         <hr>
         <li><a href="index.html" ${(location.includes('index.html') ? 'class = "link-ativo"' : '')}>Home</a></li>
-        <li><a href="#">Do Motor ao Código</a></li>
+        <li><a href="motor_codigo.html" ${(location.includes('motor_codigo.html') ? 'class = "link-ativo"' : '')}>Do Motor ao Código</a></li>
         <li><a href="games.html" ${(location.includes('games') ? 'class = "link-ativo"' : '')}>Games</a></li>
         <li><a href="dashboard.html" ${(location.includes('dashboard') ? 'class = "link-ativo"' : '')}>Dashboard</a></li>
         <li class='btn-logout' onclick='logout()'>Sair</li>
     `;
+    } else {
+        navbar.innerHTML = `
+            <li><a href="index.html" ${(location.includes('index.html') ? 'class = "link-ativo"' : '')}>Home</a></li>
+            <li><a href="motor_codigo.html" ${(location.includes('motor_codigo.html') ? 'class = "link-ativo"' : '')}>Do Motor ao Código</a></li>
+            <li><a href="login.html" class="btn-login">Login</a></li>
+            <li><a href="cadastro.html" class="btn-cadastro">Cadastro</a></li>
+      `
     }
 }
